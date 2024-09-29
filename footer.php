d3ac9de1a62f71d02c7dc50399db5d16c67cf82b
MasakiNakamoto <12nmk95@gmail.com>
2024-09-30 08:34:27 +0900

    fixed footer.php createEdit.php

--- footer.php
+++ footer.php
@@ -1,21 +1,25 @@
-    <footer class="footer">
-      <div class="footer-copy">
-        Copyright <a href="index.php">日記app</a>. All Rights Reserved.
-      </div>
+    <footer id="footer">
+      Copyright <a href="index.php">日記app</a>. All Rights Reserved.
     </footer>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script>
-      $(window).load(function() {
+      $(function() {
         // フッターを最下部に固定
-        var $ftr = $('.footer');
+        var $ftr = $('#footer');
         if (window.innerHeight > $ftr.offset().top + $ftr.outerHeight()) {
           $ftr.attr({
             'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) + 'px;'
           });
         }
+
+        // テキストエリアカウント
+        var $countUp = $('#js-count'),
+          $countView = $('#js-count-view');
+        $countUp.on('keyup', function(e) {
+          $countView.html($(this).val().length);
+        });
       });
     </script>
-
     </body>
 
     </html>
